763541ab77f156b648f0ea85f73a517bc1b560ed
João Paulo Pereira <joaopaulomatematicapho@gmail.com>
2020-11-30 23:45:28 -0300

    showing list items

--- src/App.js
+++ src/App.js
@@ -1,25 +1,43 @@
-import logo from './logo.svg';
+import React, {useState, useEffect} from 'react';
+import styled from 'styled-components';
 import './App.css';
 
+const Title = styled.h1`
+  width: auto;
+  text-align: center;
+`;
+
+const ListItem = styled.li`
+  
+`;
+
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+  const [list, setList] = useState([]);
+
+  useEffect(()=>{
+    setList([
+      {title: 'Tomar Café', done:false},
+      {title: 'Almoçar', done:false},
+      {title: 'Jantar', done:true}
+    ]);
+
+  },[]);
+
+  return (<div className='page'>
+    <Title>Lista de Tarefas</Title>
+    <hr/>
+    <ol>
+      {list.map((item, index)=>{
+        return(
+        <ListItem key={index}>{item.title}</ListItem>
+        //sempre que usamos a função map obragatoriamente temos que definir uma key
+        );
+      })}
+    </ol>
+
+  </div>);
+   
 }
 
 export default App;
